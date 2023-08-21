2efeb499d5389a29d5513a37bddd316fda095de2
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-08-21 16:29:53 +0300

    remove duplicated data

--- src/Components/Repos.js
+++ src/Components/Repos.js
@@ -1,12 +1,32 @@
-import useFetchDataFrom from "../Hooks/useFetchDataFrom";
 import { staticReposData } from "../Data/staticReposData";
+import useFetchDataFrom from "../Hooks/useFetchDataFrom";
 import Project from "./Project";
 import utility from "./_UtilityClasses.module.scss";
 
 const Repos = () => {
-  let reposData = useFetchDataFrom("https://api.github.com/users/Moamal-2000/repos");
+  let reposData = useFetchDataFrom(
+    "https://api.github.com/users/Moamal-2000/repos"
+  );
 
-  if (reposData) reposData = [...reposData, ...staticReposData];
+  function removeDuplicatesByProperty(array, property) {
+    const seen = new Set();
+
+    const uniqueObjects = array.filter((item) => {
+      if (!seen.has(item[property])) {
+        seen.add(item[property]);
+        return true;
+      }
+      return false;
+    });
+
+    return uniqueObjects;
+  }
+
+  if (reposData) {
+    reposData = [...[...reposData, ...staticReposData]];
+    const uniqueArray = removeDuplicatesByProperty(reposData, "name");
+    reposData = uniqueArray;
+  }
 
   return (
     <div className="container">
