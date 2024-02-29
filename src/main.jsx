f1c557387b1f247db690051154849c9b475b20a2
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-02-29 13:26:07 +0300

    setup PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./Styles/main.scss";
+import * as serviceWorker from "../registerPWA.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+serviceWorker.register()
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugin } from "./manifest";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   build: {
     sourcemap: true,
   },
